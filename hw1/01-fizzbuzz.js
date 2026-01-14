73439af2729172bde27a967c6f4841793349eec5
tweed1 <148729136+tweed1@users.noreply.github.com>
2026-01-14 14:32:25 -0800

    fizzbuzz complete and installed js standard
    going to try out js standard but its formatting doesn't match up with how I have prettier set up so I we'll see

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,13 +1,29 @@
 /** Exercise 01 - Fizzbuzz
 
-Write a program that writes all the numbers from 1 to 100, with some exceptions: 
-- For numbers divisible by 3, print “fizz” 
-- For numbers divisible by 5 (but not 3), print “buzz” 
+Write a program that writes all the numbers from 1 to 100, with some exceptions:
+- For numbers divisible by 3, print “fizz”
+- For numbers divisible by 5 (but not 3), print “buzz”
 - For numbers divisible by 3 and 5, print “fizzbuzz”
 
 Use console.log() to write the proper output to the command line.
 
 **/
+
+const fizzbuzz = function fizzbuzzCalculations(num) {
+	for (let i = 1; i < num; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log('fizzbuzz');
+		} else if (i % 3 === 0) {
+			console.log('fizz');
+		} else if (i % 5 === 0) {
+			console.log('buzz');
+		} else {
+			console.log(i);
+		}
+	}
+};
+
+fizzbuzz(100);
 
 // 1
 // 2
